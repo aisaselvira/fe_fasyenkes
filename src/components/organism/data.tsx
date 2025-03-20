1d4c6580ab7b5128d9899ddb5fd1efb121dbb922
Muflih Raihan <muflihraihan693@gmail.com>
2025-03-20 14:02:18 +0700

    feat: sider bar dan data dumy

---
+++ src/components/organism/data.tsx
@@ -0,0 +1,103 @@
+const patientData = [
+    {
+        id: 1,
+        jenisPasien: "Pasien Lama",
+        jenisKunjungan: "Rawat Jalan",
+        diagnosis: "Nasal Polip",
+        kasus:
+            "Pasien datang ingin periksa terkait penyakitnya yaitu Nasal Polip sudah datang dengan menggunakan rujukan baru",
+        metodePembayaran: "BPJS",
+    },
+    {
+        id: 2,
+        jenisPasien: "Pasien Lama",
+        jenisKunjungan: "Rawat Jalan",
+        diagnosis: "Nasal Polip",
+        kasus:
+            "Pasien datang ingin periksa terkait penyakitnya yaitu Nasal Polip sudah datang dengan menggunakan rujukan baru",
+        metodePembayaran: "BPJS",
+    },
+    {
+        id: 3,
+        jenisPasien: "Pasien Lama",
+        jenisKunjungan: "Rawat Jalan",
+        diagnosis: "Nasal Polip",
+        kasus:
+            "Pasien datang ingin periksa terkait penyakitnya yaitu Nasal Polip sudah datang dengan menggunakan rujukan baru",
+        metodePembayaran: "BPJS",
+    },
+    {
+        id: 4,
+        jenisPasien: "Pasien Lama",
+        jenisKunjungan: "Rawat Jalan",
+        diagnosis: "Nasal Polip",
+        kasus:
+            "Pasien datang ingin periksa terkait penyakitnya yaitu Nasal Polip sudah datang dengan menggunakan rujukan baru",
+        metodePembayaran: "BPJS",
+    },
+    {
+        id: 5,
+        jenisPasien: "Pasien Lama",
+        jenisKunjungan: "Rawat Jalan",
+        diagnosis: "Nasal Polip",
+        kasus:
+            "Pasien datang ingin periksa terkait penyakitnya yaitu Nasal Polip sudah datang dengan menggunakan rujukan baru",
+        metodePembayaran: "BPJS",
+    },
+    {
+        id: 6,
+        jenisPasien: "Pasien Lama",
+        jenisKunjungan: "Rawat Jalan",
+        diagnosis: "Nasal Polip",
+        kasus:
+            "Pasien datang ingin periksa terkait penyakitnya yaitu Nasal Polip sudah datang dengan menggunakan rujukan baru",
+        metodePembayaran: "BPJS",
+    },
+    {
+        id: 7,
+        jenisPasien: "Pasien Lama",
+        jenisKunjungan: "Rawat Jalan",
+        diagnosis: "Nasal Polip",
+        kasus:
+            "Pasien datang ingin periksa terkait penyakitnya yaitu Nasal Polip sudah datang dengan menggunakan rujukan baru",
+        metodePembayaran: "BPJS",
+    },
+    {
+        id: 8,
+        jenisPasien: "Pasien Lama",
+        jenisKunjungan: "Rawat Jalan",
+        diagnosis: "Nasal Polip",
+        kasus:
+            "Pasien datang ingin periksa terkait penyakitnya yaitu Nasal Polip sudah datang dengan menggunakan rujukan baru",
+        metodePembayaran: "BPJS",
+    },
+    {
+        id: 9,
+        jenisPasien: "Pasien Lama",
+        jenisKunjungan: "Rawat Jalan",
+        diagnosis: "Nasal Polip",
+        kasus:
+            "Pasien datang ingin periksa terkait penyakitnya yaitu Nasal Polip sudah datang dengan menggunakan rujukan baru",
+        metodePembayaran: "BPJS",
+    },
+    {
+        id: 10,
+        jenisPasien: "Pasien Lama",
+        jenisKunjungan: "Rawat Jalan",
+        diagnosis: "Nasal Polip",
+        kasus:
+            "Pasien datang ingin periksa terkait penyakitnya yaitu Nasal Polip sudah datang dengan menggunakan rujukan baru",
+        metodePembayaran: "BPJS",
+    },
+    {
+        id: 11,
+        jenisPasien: "Pasien baru",
+        jenisKunjungan: "Rawat Jalan",
+        diagnosis: "Nasal Polip",
+        kasus:
+            "Pasien datang ingin periksa terkait penyakitnya yaitu Nasal Polip sudah datang dengan menggunakan rujukan baru",
+        metodePembayaran: "BPJS",
+    },
+]
+
+export default patientData;
