da55adbe5a0299d810a8cf60e75b223504ad4004
Aisa Selvira <aisaselvira2020@gmail.com>
2025-03-22 20:47:12 +0700

    update sidebar-admin

--- src/components/organism/sidebar-admin.tsx
+++ src/components/organism/sidebar-admin.tsx
@@ -14,6 +14,7 @@ interface MenuItem {
     icon: ReactNode;
 }
 
+
 const menuItems: MenuItem[] = [
     {label: "Dashboard", href: "/admin/dasboard/page", icon: <LayoutDashboard size={20} />},
     {label: "Kelola Simulasi TPPRJ", href: "/admin/simulasi-tpprj/page", icon: <Settings size={20} />},
