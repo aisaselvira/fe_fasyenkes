be1afd7f5b1e709fb5b2046f59b14f2e372bdf5f
M2rei <124634820+M2rei@users.noreply.github.com>
2025-04-16 08:09:58 +0700

    Fe rei (#21)
    * feat:
    
    * feat: simulasi page, dasboard, login feach
    
    * feat: rubah tampilan dasboard, simulasi
    
    * fea: login, dasboard
    
    * feat: skenario

--- src/components/template/admin/page-skenario/Skenario-tppgd.tsx
+++ src/components/template/admin/page-skenario/Skenario-tppgd.tsx
@@ -140,9 +140,11 @@ export default function KelolaSkenarioTppgd() {
                                                 </TableCell>
                                                 <TableCell className="whitespace-nowrap">
                                                     <div className="flex justify-center space-x-2">
-                                                        <button className="p-1 hover:text-yellow-600" aria-label="Lihat Detail">
-                                                            <Eye className="h-4 w-4 md:h-5 md:w-5" />
-                                                        </button>
+                                                        <Link href={`/admin/simulasi-tppgd/${simulasiid}/show/${scenario.id}`}>
+                                                            <button className="p-1 hover:text-yellow-600" aria-label="Lihat Detail">
+                                                                <Eye className="h-4 w-4 md:h-5 md:w-5" />
+                                                            </button>
+                                                        </Link>
                                                         <button className="p-1 hover:text-blue-800" aria-label="Edit">
                                                             <Edit className="h-4 w-4 md:h-5 md:w-5" />
                                                         </button>
